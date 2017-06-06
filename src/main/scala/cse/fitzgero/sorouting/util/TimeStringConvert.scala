ddff3ad9deaa06a343f85aa6545705a1987498de
Rob Fitzgerald <robfitzgerald@gmail.com>
2017-06-06 14:21:03 -0600

    feature-1 Extend MATSim with a utility to generate link flow snapshots
    TimeStringConvert completed.

--- src/main/scala/cse/fitzgero/sorouting/util/TimeStringConvert.scala
+++ src/main/scala/cse/fitzgero/sorouting/util/TimeStringConvert.scala
@@ -3,27 +3,49 @@ package cse.fitzgero.sorouting.util
 import scala.util.{Try, Success, Failure}
 
 object TimeStringConvert {
+  val TimeUpperBound: Int = 86400
+  val HourBase: Int = 24
+  val HourStep: Int = 3600
+  val MinStep: Int = 60
+  val MinBase: Int = 60
+  val SecBase: Int = 60
+  val TimeLowerBound: Int = 0
+
   def fromString(s: String): Int = s.split(":") match {
     case Array(hh: String,mm: String,ss: String) => stringsInBounds(hh,mm,ss) match {
       case Success(true) => stringToInt(hh, mm, ss)
-      case _ => throw new java.lang.ArithmeticException(s"String $s does not conform to 24-hour clock specification - failed bounds test.")
+      case Success(false) => throw new java.lang.ArithmeticException(s"String $s does not conform to 24-hour clock specification - failed bounds test.")
+      case _ => throw new java.lang.ArithmeticException(s"String $s does not conform to 24-hour clock specification - values were not integers.")
     }
     case _ => throw new java.lang.ArithmeticException(s"String $s does not conform to 24-hour clock specification - failed partitioning.")
   }
 
   def fromInt(i: Int): String = {
-    ""
+    if (intInBounds(i)) intToString(i)
+    else throw new java.lang.ArithmeticException(s"Int $i does not conform to 24-hour clock specification - failed bounds test [0, 86400).")
+  }
+
+  private def intToString(i: Int): String = {
+    val hhVal: String = (i / HourStep).toString
+    val mmVal: String = ((i % HourStep) / MinStep).toString
+    val ssVal: String = (i % MinStep).toString
+    val hh: String = if (hhVal.length == 1) "0" + hhVal else hhVal
+    val mm: String = if (mmVal.length == 1) "0" + mmVal else mmVal
+    val ss: String = if (ssVal.length == 1) "0" + ssVal else ssVal
+    s"$hh:$mm:$ss"
   }
 
   private def stringToInt(hh: String, mm: String, ss: String): Int = {
-    (hh.toInt * 3600) + (mm.toInt * 60) + ss.toInt
+    (hh.toInt * HourStep) + (mm.toInt * MinStep) + ss.toInt
   }
 
   private def stringsInBounds (hh: String, mm: String, ss: String): Try[Boolean] = {
     Try({
-      0 <= hh.toInt && hh.toInt < 24 &&
-      0 <= mm.toInt && mm.toInt < 60 &&
-      0 <= ss.toInt && ss.toInt < 60
+      TimeLowerBound <= hh.toInt && hh.toInt < HourBase &&
+      TimeLowerBound <= mm.toInt && mm.toInt < MinBase &&
+      TimeLowerBound <= ss.toInt && ss.toInt < SecBase
     })
   }
+
+  private def intInBounds (i: Int): Boolean = (TimeLowerBound until TimeUpperBound).contains(i)
 }
--- src/test/scala/cse/fitzgero/sorouting/util/TimeStringConvertTests.scala
+++ src/test/scala/cse/fitzgero/sorouting/util/TimeStringConvertTests.scala
@@ -15,12 +15,24 @@ class TimeStringConvertTests extends SORoutingUnitTests with PrivateMethodTester
         TimeStringConvert.fromString("23:59:59") should equal (86399)
       }
       "throw an error for out of lower bounds time strings" in {
-        val thrown = the [java.lang.ArithmeticException] thrownBy TimeStringConvert.fromString("-00:00:00")
-        thrown.getMessage should equal ("something logical")
+        val badTime: String = "-01:00:00"
+        val thrown = the [java.lang.ArithmeticException] thrownBy TimeStringConvert.fromString(badTime)
+        thrown.getMessage should equal (s"String $badTime does not conform to 24-hour clock specification - failed bounds test.")
       }
       "throw an error for out of upper bounds time strings" in {
-        val thrown = the [java.lang.ArithmeticException] thrownBy TimeStringConvert.fromString("24:00:00")
-        thrown.getMessage should equal ("something logical")
+        val badTime: String = "24:00:00"
+        val thrown = the [java.lang.ArithmeticException] thrownBy TimeStringConvert.fromString(badTime)
+        thrown.getMessage should equal (s"String $badTime does not conform to 24-hour clock specification - failed bounds test.")
+      }
+      "throw an error for values which are not parseable into integers" in {
+        val badTime: String = "hh:00:00"
+        val thrown = the [java.lang.ArithmeticException] thrownBy TimeStringConvert.fromString(badTime)
+        thrown.getMessage should equal (s"String $badTime does not conform to 24-hour clock specification - values were not integers.")
+      }
+      "throw an error for values which cannot be broken into 3 parts by the : separator" in {
+        val badTime: String = "01:30"
+        val thrown = the [java.lang.ArithmeticException] thrownBy TimeStringConvert.fromString(badTime)
+        thrown.getMessage should equal (s"String $badTime does not conform to 24-hour clock specification - failed partitioning.")
       }
     }
     "fromInt" should {
@@ -32,22 +44,34 @@ class TimeStringConvertTests extends SORoutingUnitTests with PrivateMethodTester
         TimeStringConvert.fromInt(86399) should equal ("23:59:59")
       }
       "throw an error for out of lower bounds time values" in {
-        val thrown = the [java.lang.ArithmeticException] thrownBy TimeStringConvert.fromInt(-1)
-        thrown.getMessage should equal ("something logical")
+        val badTime: Int = -1
+        val thrown = the [java.lang.ArithmeticException] thrownBy TimeStringConvert.fromInt(badTime)
+        thrown.getMessage should equal (s"Int $badTime does not conform to 24-hour clock specification - failed bounds test [0, 86400).")
       }
       "throw an error for out of upper bounds time values" in {
-        val thrown = the [java.lang.ArithmeticException] thrownBy TimeStringConvert.fromInt(86400)
-        thrown.getMessage should equal ("something logical")
+        val badTime: Int = 86400
+        val thrown = the [java.lang.ArithmeticException] thrownBy TimeStringConvert.fromInt(badTime)
+        thrown.getMessage should equal (s"Int $badTime does not conform to 24-hour clock specification - failed bounds test [0, 86400).")
       }
     }
     "stringToInt" should {
-      "correctly covert string values to int values" in {
+      "correctly convert string values to int values" in {
         val stringToInt = PrivateMethod[Int]('stringToInt)
         TimeStringConvert invokePrivate stringToInt("00","00","00") should equal (0)
         TimeStringConvert invokePrivate stringToInt("06","00","00") should equal (21600)
         TimeStringConvert invokePrivate stringToInt("06","00","01") should equal (21601)
         TimeStringConvert invokePrivate stringToInt("06","01","00") should equal (21660)
         TimeStringConvert invokePrivate stringToInt("23","59","59") should equal (86399)
+      }
+    }
+    "intToString" should {
+      "correctly convert int values to string values" in {
+        val intToString = PrivateMethod[String]('intToString)
+        TimeStringConvert invokePrivate intToString(0) should equal ("00:00:00")
+        TimeStringConvert invokePrivate intToString(21600) should equal ("06:00:00")
+        TimeStringConvert invokePrivate intToString(21601) should equal ("06:00:01")
+        TimeStringConvert invokePrivate intToString(21660) should equal ("06:01:00")
+        TimeStringConvert invokePrivate intToString(86399) should equal ("23:59:59")
       }
     }
     "stringsInBounds" should {
@@ -65,6 +89,18 @@ class TimeStringConvertTests extends SORoutingUnitTests with PrivateMethodTester
         TimeStringConvert invokePrivate stringsInBounds("-2","00","00") should equal (Success(false))
         TimeStringConvert invokePrivate stringsInBounds("00","65","00") should equal (Success(false))
         TimeStringConvert invokePrivate stringsInBounds("00","00","100") should equal (Success(false))
+      }
+    }
+    "intInBounds" should {
+      "identify values that are in range" in {
+        val intInBounds = PrivateMethod[Boolean]('intInBounds)
+        TimeStringConvert invokePrivate intInBounds(0) should equal (true)
+        TimeStringConvert invokePrivate intInBounds(86399) should equal (true)
+      }
+      "identify values out of range" in {
+        val intInBounds = PrivateMethod[Boolean]('intInBounds)
+        TimeStringConvert invokePrivate intInBounds(-1) should equal (false)
+        TimeStringConvert invokePrivate intInBounds(86400) should equal (false)
       }
     }
   }
