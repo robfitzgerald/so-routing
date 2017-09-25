1d63e740fbc8102ef8c4e08600baabc5fb69dab9
Rob Fitzgerald <robfitzgerald@gmail.com>
2017-09-25 17:22:10 -0600

    some comments and cleanup on the test

--- src/test/scala/cse/fitzgero/sorouting/algorithm/pathselection/localgraph/LocalGraphPathSelectionTests.scala
+++ src/test/scala/cse/fitzgero/sorouting/algorithm/pathselection/localgraph/LocalGraphPathSelectionTests.scala
@@ -35,10 +35,13 @@ class LocalGraphPathSelectionTests extends SORoutingAsyncUnitTestTemplate {
         val PathSelection = LocalGraphPathSelection()
         PathSelection.run(requests, graph) map {
           case LocalGraphPathSelectionResult(paths, runTime) =>
-            println(paths)
-            println(runTime)
+            // there should be one result per person: 2 total
             paths.size should equal (2)
+            // person 2's alternates were very poor. it should force them to select the path through -[2]->-[11]->
             paths.filter(_.personId == "2").head.path should equal(List[EdgeId]("1","2","11","20","21","22"))
+            // because person 2 was forced to -[2]->-[11]->, it means person 1 should be pushed to -[3]->-[12]->
+            // since the combined congestion effect of sending two vehicles on -[2]->-[11]-> is greater
+            paths.filter(_.personId == "1").head.path should equal(List[EdgeId]("1","3","12","20","21","22"))
           case _ => fail()
         }
       }
