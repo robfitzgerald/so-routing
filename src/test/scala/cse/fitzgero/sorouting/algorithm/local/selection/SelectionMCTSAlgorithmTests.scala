e9a5dcc26a62502745dd6477ecba1ef63caa1224
Rob Fitzgerald <robfitzgerald@gmail.com>
2017-11-14 14:40:44 -0700

    small test changes.

--- src/test/scala/cse/fitzgero/sorouting/algorithm/local/selection/SelectionMCTSAlgorithmTests.scala
+++ src/test/scala/cse/fitzgero/sorouting/algorithm/local/selection/SelectionMCTSAlgorithmTests.scala
@@ -11,7 +11,7 @@ class SelectionMCTSAlgorithmTests extends SORoutingUnitTestTemplate {
       "called with a small graph and two small sets of alternate paths" should {
         "find the optimal combination" in new CombinatorialTestAssets.CombinationSet {
           case class Config(coefficientCp: Double, congestionRatioThreshold: Double, computationalLimit: Long)
-          val config = Config(0.7071D, 3D, 5000)
+          val config = Config(0.7071D, 3D, 2000)
           SelectionMCTSAlgorithm.runAlgorithm(graph, kspResult, Some(config)) match {
             case None => fail()
             case Some(result) =>
@@ -22,7 +22,7 @@ class SelectionMCTSAlgorithmTests extends SORoutingUnitTestTemplate {
       "called with a bigger graph and set of alternate paths" should {
         "find the optimal combination" in new CombinatorialTestAssets.BiggerMap {
           case class Config(coefficientCp: Double, congestionRatioThreshold: Double, computationalLimit: Long)
-          val config = Config(0, 3.14D, 5000)
+          val config = Config(0, 3.14D, 2000)
           SelectionMCTSAlgorithm.runAlgorithm(bigGraph, kspResult, Some(config)) match {
             case None => fail()
             case Some(result) =>
