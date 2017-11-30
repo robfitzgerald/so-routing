4c4a524444313232cb93929150aa205eba05d84f
Rob Fitzgerald <robfitzgerald@gmail.com>
2017-11-30 12:19:50 -0700

    congestion threshold argument added for UE test

--- src/main/scala/cse/fitzgero/sorouting/experiments/MATSimUETest.scala
+++ src/main/scala/cse/fitzgero/sorouting/experiments/MATSimUETest.scala
@@ -13,10 +13,11 @@ object MATSimUETest extends Experiment with App with MATSimSimulator {
   val pop: Int = if (args.length > 0) args(0).toInt else 100
   val win: Int = if (args.length > 1) args(1).toInt else 15
   val route: Double = if (args.length > 2) args(2).toDouble else 0.20D
-  val name: String = if (args.length > 3) args(3) else "test"
-  val sourceAssetDirectory: String = if (args.length > 4) args(4) else "data/5x5"
+  val congestionThreshold: Double = if (args.length > 3) args(3).toDouble else 1.50D
+  val name: String = if (args.length > 4) args(4) else "mctsTest"
+  val sourceAssetDirectory: String = if (args.length > 5) args(5) else "data/5x5"
   val sourceAssetName: String = Paths.get(sourceAssetDirectory).getFileName.toString
-  val configLabel: String = s"$sourceAssetName-$pop-$win-$route"
+  val configLabel: String = s"$sourceAssetName-$pop-$win-$route-$congestionThreshold"
   
   case class Config (
     sourceAssetsDirectory: String,
